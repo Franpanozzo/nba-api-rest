ec0ac57ea8d70d04f3a4cff0c7da960a42f75db1
Franpanozzo <francisco.panozzosf@gmail.com>
2022-04-27 21:52:37 -0300

    aws secret and clean comments

--- handler.js
+++ handler.js
@@ -14,13 +14,3 @@ async function conectarDB() {
 }
 
 module.exports.handler = (conectarDB()) ? serverless(app) : null;
-
-// module.exports.handler = async (event) => {
-//   await mongoConnect(process.env.MONGO_URL);
-//   console.log('Ya estamos coenctados!');
-//   return serverless(app);
-// }
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-
